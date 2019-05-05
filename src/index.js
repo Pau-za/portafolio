ad2ed0af8bd3031307a8ed01edaa126d8bca7445
Pau-za <molo_pau@hotmail.com>
2019-05-04 22:33:52 -0500

    agrega nav y su contenido al index.html. Ahora voy a trabajar en el contenido de las secciones

---
+++ src/index.js
@@ -0,0 +1,7 @@
+console.log('hola mundo');
+// const dropdown = document.getElementsByClassName('dropdown-trigger');
+// console.log(dropdown);
+// dropdown.addEventListener('click', () => {
+//     document.querySelectorAll('.dropdown-content').style.display = 'block';
+//   })
+  
